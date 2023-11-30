9aa2ea8ca849876454e641e879242e1315dd954b
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2023-11-30 16:31:53 +0900

    Update 0550-game-play-analysis-iv.sql

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,12 +1,3 @@
-# Write your MySQL query statement below
-
-# with temp as
-# (select *
-# from Activity)
-#     select round(count(distinct player_id) / (select count(distinct player_id) from temp), 2) as fraction
-#     from Activity
-#     where (player_id, event_date) IN (select player_id, DATE_ADD(min(event_date), INTERVAL 1 DAY) as nextday_first_log from Activity group by player_id)
-
 With Temp AS
 (SELECT *
 FROM Activity)
