ce0d41f09db965fa9687a549c6720f2225580fd8
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2023-12-24 09:07:21 +0900

    Time: 630 ms (68.69%), Space: 0B (100.00%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT visited_on
+     , amount
+     , ROUND(amount/7,2) AS average_amount
+FROM (SELECT DISTINCT visited_on
+           , SUM(amount) OVER(ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) AS amount
+      FROM Customer) AS sum_amount
+WHERE visited_on >= (SELECT MIN(visited_on) FROM Customer) + INTERVAL 6 DAY
+
